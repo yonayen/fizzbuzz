16c79a93e2d98a57565c82c0cec9bc841e256547
Yonathan Ayenew <yonayen@gmail.com>
2016-11-01 10:26:31 -0700

    change comment on document

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -16,13 +16,12 @@ $(document).ready(function() {
 });
 
 
- // -- Assignment --//
+ -- Assignment --//
 
-// Count from 1-100; just say number if it's not divisible by 3 or 5.  
-// If divisible by 3 --> 'fizz'
-// If divisible by 5 --> 'buzz'
-// If dibisible by both 3 & 5 --> 'fizzbuzz'
-// 1, 2, fizz, 4, buzz, fizz, 7,8,fizz, buzz, 11, fizz, 13, 14, fizz buzz
+Count from 1-100; just say number if it's not divisible by 3 or 5.
+If divisible by 3 --> 'fizz'
+If divisible by 5 --> 'buzz'
+If dibisible by both 3 & 5 --> 'fizzbuzz'
+1, 2, fizz, 4, buzz, fizz, 7,8,fizz, buzz, 11, fizz, 13, 14, fizz buzz
 
- // ------- //
-
+ ------- //
